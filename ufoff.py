fe9fd07a44a5811aa0b5b3129af97518de94d115
decc4l888 <dcabukel8@gmail.com>
2025-04-18 20:14:42 +0300

    doluluk.py

--- ufoff.py
+++ ufoff.py
@@ -1,20 +1,11 @@
 import trimesh
 
-# STL'yi yükle
 mesh = trimesh.load_mesh("iha.stl")
 
-# Hacim (mm³ -> cm³)
 volume_cm3 = mesh.volume / 1000
-
-# ASA yoğunluğu
-density = 1.07  # g/cm³
-
-# Hedef toplam ağırlık (örneğin 300 gram altında kalmak istiyoruz)
+density = 1.07  
 target_weight = 1025
-# Gereken doluluk oranını hesapla
 required_infill = target_weight / (volume_cm3 * density)
-
-# Oran %100'ü geçiyorsa bu tasarımı hafifletmen gerekir
 if required_infill > 1:
     print("Tasarım çok ağır, yeniden tasarlaman gerekebilir.")
 else:
